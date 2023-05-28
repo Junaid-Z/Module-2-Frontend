64aacf73605a43df7c9a8a53454d759768483747
Junaid-Z <jzafar828@gmail.com>
2023-05-28 23:25:43 +0500

    Old Code Deleted And Icon Changed

--- src/pages/Put-Post/Teacher.jsx
+++ src/pages/Put-Post/Teacher.jsx
@@ -1,50 +1,6 @@
-// import axios from 'axios';
-// import React, { useState } from 'react'
-
-// const Teacher = () => {
-//   let [inputs, setInputs] = useState({})
-//   function handleIInput(e) {
-//     setInputs((prev) => {
-//       prev[e.target.name] = e.target.value;
-//       return { ...prev }
-//     })
-//   }
-//   return (<div className='studentFormHolder'>
-//     <form className='studentForm' onSubmit={(e) => { e.preventDefault() }}>
-//       <label>
-//         Name:
-//         <input value={inputs.name || ''} name='name' onChange={handleIInput} />
-//       </label>
-//       <label>
-//         Course:
-//         <input value={inputs.course || ''} name='course' onChange={handleIInput} />
-//       </label>
-//       <label>
-//         Contact:
-//         <input value={inputs.contact || ''} name='contact' onChange={handleIInput} />
-//       </label>
-//       <button
-//         onClick={(e) => {
-//           axios.post('http://localhost:2121/teachers', inputs)
-//             .then(() => {
-//               setInputs({})
-//             })
-//             .catch((e) => {
-//               console.log(e)
-//             })
-//           e.preventDefault()
-//         }} >
-//         Submit
-//       </button>
-//     </form>
-//   </div>)
-// }
-
-// export default Teacher
-
 import React from 'react'
 import PostPage from '../../components/PostPage'
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 
 const AddTeacher = () => {
 	return (
@@ -67,7 +23,7 @@ const AddTeacher = () => {
 				]
 			}
 			endpoint={"teachers"}
-			logo={<PersonAddIcon className='studentLogo' />} />
+			logo={<AssignmentIndIcon className='studentLogo' />} />
 	)
 }
 
