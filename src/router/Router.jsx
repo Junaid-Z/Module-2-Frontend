6cb2fd18786e43689ca233884813f8a81042f521
Junaid-Z <jzafar828@gmail.com>
2023-05-28 23:26:16 +0500

    Structure Changed

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,42 +1,51 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AddStudent from '../pages/Post/Student'
-import Student from '../pages/Get/Student'
+import ResponsiveDrawer from '../components/ResponsiveDrawer'
+import '../Post.css'
+import '../Navbar.css'
+
 import Courses from '../pages/Get/Course'
-import AddCourse from '../pages/Post/Course'
-import TeacherPage from '../pages/Get/Teacher'
-import Teacher from '../pages/Post/Teacher'
-import LinkList from '../pages/LinkList'
-import '../listPage.css'
-import '../addForm.css'
-import '../LinkListPage.css'
-import EditCourse from '../pages/Put/Course'
-import EditTeacher from '../pages/Put/Teacher'
-import EditStudent from '../pages/Put/Student'
-import Institute from '../pages/Post/Institute'
-import EditInstitute from '../pages/Put/Institute'
-import InstitutePage from '../pages/Get/Institute'
+import Teachers from '../pages/Get/Teacher'
+import Institutes from '../pages/Get/Institute'
+import Students from '../pages/Get/Student'
+
+import AddStudent from '../pages/Put-Post/Student'
+import AddCourse from '../pages/Put-Post/Course'
+import AddTeacher from '../pages/Put-Post/Teacher'
+import AddInstitute from '../pages/Put-Post/Institute'
+
+import links from './NavLinks.json'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LinkList />} />
-        <Route path='/addStudent' element={<AddStudent />} />
-        <Route path='/editStudent/:_id' element={<EditStudent />} />
-        <Route path='/students' element={<Student />} />
+        <Route path='/' element={<ResponsiveDrawer sideLinks={links} />} >
+          <Route path='/students' element={<Students />} />
+          <Route path='/addStudent' element={<AddStudent />} >
+            <Route path='/addStudent/'></Route>
+            <Route path='/addStudent/:_id'></Route>
+          </Route>
 
-        <Route path='/addCourse' element={<AddCourse />} />
-        <Route path='/editCourse/:_id' element={<EditCourse />} />
-        <Route path='/courses' element={<Courses />} />
+          <Route path='/courses' element={<Courses />} />
+          <Route path='/addCourse' element={<AddCourse />} >
+            <Route path='/addCourse/'></Route>
+            <Route path='/addCourse/:_id'></Route>
+          </Route>
 
-        <Route path='/addTeacher' element={<Teacher />} />
-        <Route path='/editTeacher/:_id' element={<EditTeacher />} />
-        <Route path='/teachers' element={<TeacherPage />} />
+          <Route path='/teachers' element={<Teachers />} />
+          <Route path='/addTeacher' element={<AddTeacher />} >
+            <Route path='/addTeacher/'></Route>
+            <Route path='/addTeacher/:_id'></Route>
+          </Route>
 
-        <Route path='/addInstitute' element={<Institute />} />
-        <Route path='/editInstitute/:_id' element={<EditInstitute />} />
-        <Route path='/institutes' element={<InstitutePage />} />
+          <Route path='/institutes' element={<Institutes />} />
+          <Route path='/addInstitute' element={<AddInstitute />} >
+            <Route path='/addInstitute/'></Route>
+            <Route path='/addInstitute/:_id'></Route>
+          </Route>
+
+        </Route>
       </Routes>
     </BrowserRouter>
   )
